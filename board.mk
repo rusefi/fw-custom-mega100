264d2ce68c894776b83a797b5b18aee3f347d236
rusefillc <sdfsdfqsf2334234234>
2023-12-04 10:38:10 -0500

    Hello World

--- board.mk
+++ board.mk
@@ -4,4 +4,5 @@ DDEFS += -DFIRMWARE_ID=\"XXX\"
 SHORT_BOARD_NAME=XXX
 
 # assign critical LED to a non-existent pin if you do not have it on your board
-DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# good old PD14 is still the default value
+# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
