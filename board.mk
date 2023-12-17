ea24f7b7d723a014d7ca579f7b04d57cd1391817
rusefillc <48498823+rusefillc@users.noreply.github.com>
2023-12-16 20:25:36 -0500

    Update board.mk

--- board.mk
+++ board.mk
@@ -3,7 +3,8 @@ BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 DDEFS += -DFIRMWARE_ID=\"XXX\"
 SHORT_BOARD_NAME=XXX
 
-DDEFS += -DRAM_UNUSED_SIZE=1000
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=100
 
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
