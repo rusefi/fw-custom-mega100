bae88d204aa33d307ce901abfe805bbffd593f46
rusefillc <48498823+rusefillc@users.noreply.github.com>
2023-12-16 20:18:00 -0500

    Update board.mk

--- board.mk
+++ board.mk
@@ -3,6 +3,8 @@ BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 DDEFS += -DFIRMWARE_ID=\"XXX\"
 SHORT_BOARD_NAME=XXX
 
+DDEFS += -DRAM_UNUSED_SIZE=1000
+
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
 # DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
