dc8a99fdf6632fc9d0f2f5b0fa8110e79ac6a525
Andrey <rusefillc@gmail.com>
2023-12-20 09:26:09 -0500

    olf files

--- generated/signature_XXX.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 2366587278
-#define TS_SIGNATURE "rusEFI default.2023.12.17.XXX.2366587278"
