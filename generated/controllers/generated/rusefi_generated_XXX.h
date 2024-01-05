2d1a07c394c91d17e6912c64663a56b6f692977b
GitHub git update Action <action@github.com>
2024-01-05 17:17:35 +0000

    GHA Automation

--- generated/controllers/generated/rusefi_generated_XXX.h
+++ generated/controllers/generated/rusefi_generated_XXX.h
@@ -1151,7 +1151,7 @@
 #define show_microRusEFI_presets false
 #define show_Proteus_presets false
 #define show_test_presets false
-#define SIGNATURE_HASH 4099975782
+#define SIGNATURE_HASH 4158129036
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1465,7 +1465,7 @@
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
 #define ts_show_vr_threshold_pins true
-#define TS_SIGNATURE "rusEFI default.2024.01.04.XXX.4099975782"
+#define TS_SIGNATURE "rusEFI default.2024.01.05.XXX.4158129036"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_XXX.h
+++ generated/controllers/generated/signature_XXX.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4099975782
-#define TS_SIGNATURE "rusEFI default.2024.01.04.XXX.4099975782"
+#define SIGNATURE_HASH 4158129036
+#define TS_SIGNATURE "rusEFI default.2024.01.05.XXX.4158129036"
