44d1903bb1a4540edeb5c519fdecf69cae9bae4f
andreika-git <andrei.home@gmail.com>
2024-01-24 20:34:08 +0200

    Make manual-generate-meta-data.sh use meta-info.env

--- manual-generate-meta-data.sh
+++ manual-generate-meta-data.sh
@@ -5,9 +5,11 @@
 # open issue is GitHub Action should be invoking this script in order to reduce existing code duplication
 #
 
+BUNDLE_NAME="$(grep "SHORT_BOARD_NAME=" ./meta-info.env | cut -d'=' -f2)"
+
 export META_OUTPUT_ROOT_FOLDER=../../../generated/
 
 cd ext/rusefi/firmware/
 
 # todo: use meta-info.txt
-bash gen_config_board.sh ../../.. XXX
+bash gen_config_board.sh ../../.. ${BUNDLE_NAME}
