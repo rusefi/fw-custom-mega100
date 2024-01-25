f5883bcce20a74a6db66f7d2a73a18907f8338ac
GitHub git update Action <action@github.com>
2024-01-25 17:17:24 +0000

    GHA Automation

--- generated/controllers/generated/rusefi_generated_XXX.h
+++ generated/controllers/generated/rusefi_generated_XXX.h
@@ -1476,7 +1476,7 @@
 #define ts_show_vr_threshold_2 false
 #define ts_show_vr_threshold_all false
 #define ts_show_vr_threshold_pins true
-#define TS_SIGNATURE "rusEFI default.2024.01.24.XXX.2236581371"
+#define TS_SIGNATURE "rusEFI default.2024.01.25.XXX.2236581371"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
