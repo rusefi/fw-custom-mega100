7b732425ca755022d5b3cb9755aafc1a503c5eb6
rusefillc <sdfsdfqsf2334234234>
2024-04-21 09:30:53 -0400

    https://github.com/rusefi/fw-custom-example/issues/7

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -11,3 +11,34 @@ Gpio getRunningLedPin() {
 Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
+
+// board-specific configuration setup
+void setBoardDefaultConfiguration() {
+    // engineConfiguration->injectionPins[0] = Gpio::F13;
+    // engineConfiguration->ignitionPins[0] = Gpio::E15;
+
+//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
+//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
+
+//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
+
+//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
+
+//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
+
+
+    	// 5.6k high side/10k low side = 1.56 ratio divider
+  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
+
+    	// 6.34k high side/ 1k low side
+//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
+
+//	engineConfiguration->adcVcc = 3.3f;
+
+//	engineConfiguration->clt.config.bias_resistor = 2490;
+//	engineConfiguration->iat.config.bias_resistor = 2490;
+
+
+	// Battery sense on PA0
+//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
+}
