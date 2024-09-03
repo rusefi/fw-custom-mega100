053d878fc21ddceaa3a99c7d28930980b0094fdb
Andrey <rusefillc@gmail.com>
2024-09-02 23:59:50 -0400

    improving

--- compile_firmware.sh
+++ compile_firmware.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-export META_OUTPUT_ROOT_FOLDER=$(realpath --relative-to=ext/rusefi/firmware generated)
+
+
+export META_OUTPUT_ROOT_FOLDER=$(realpath --relative-to=ext/rusefi/firmware generated)/
 echo "META_OUTPUT_ROOT_FOLDER=$META_OUTPUT_ROOT_FOLDER"
+
+export AUTOMATION_REF=`git branch --show-current`
 cd ext/rusefi/firmware/
 bash bin/compile.sh ../../../meta-info.env
