eba2b6579405397845ce50bb6cd04d4f92045f63
Andrey <rusefillc@gmail.com>
2024-09-02 23:57:46 -0400

    helping local?

--- compile_firmware.sh
+++ compile_firmware.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export META_OUTPUT_ROOT_FOLDER=$(realpath --relative-to=ext/rusefi/firmware .)
+export META_OUTPUT_ROOT_FOLDER=$(realpath --relative-to=ext/rusefi/firmware generated)
 echo "META_OUTPUT_ROOT_FOLDER=$META_OUTPUT_ROOT_FOLDER"
 cd ext/rusefi/firmware/
 bash bin/compile.sh ../../../meta-info.env
