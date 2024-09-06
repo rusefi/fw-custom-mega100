b2b25f36bbc3afe94498f73c3c521b29b06b729f
rusefillc <48498823+rusefillc@users.noreply.github.com>
2024-09-06 11:33:35 -0400

    Update board.mk

--- board.mk
+++ board.mk
@@ -10,6 +10,5 @@ include $(BOARD_DIR)/meta-info.env
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# assign critical LED to a non-existent pin
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
