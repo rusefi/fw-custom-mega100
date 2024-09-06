7b27a0d59dd49a0d80fe4b5301aea195a2a2501d
GitHub git update Action <action@github.com>
2024-09-06 18:01:53 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_mega100.h
+++ generated/controllers/generated/signature_mega100.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 821184268
-#define TS_SIGNATURE "rusEFI main.2024.09.06.mega100.821184268"
+#define SIGNATURE_HASH 2965644658
+#define TS_SIGNATURE "rusEFI main.2024.09.06.mega100.2965644658"
