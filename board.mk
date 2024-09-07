ac70231fbec57130eddd4516ddb258291536eb9e
Andrey <rusefillc@gmail.com>
2024-09-07 00:37:49 -0400

    per-board pinout

--- board.mk
+++ board.mk
@@ -1,4 +1,5 @@
 BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
+    $(BOARD_DIR)/firmware/pinouts.cpp \
     $(BOARD_DIR)/default_tune.cpp \
 
 
---
+++ firmware/pinouts.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+
+static void setBoard_04_pinout() {
+    engineConfiguration->injectionPins[0] = Gpio::D8;
+    engineConfiguration->injectionPins[1] = Gpio::D9;
+    engineConfiguration->injectionPins[2] = Gpio::D10;
+    engineConfiguration->injectionPins[3] = Gpio::D11;
+}
+
+static void setBoard_ua4c_pinout() {
+    engineConfiguration->injectionPins[0] = Gpio::D8;
+    engineConfiguration->injectionPins[0] = Gpio::B15;
+}
+
+/*PUBLIC_API_WEAK*/ void boardTsAction(uint16_t index) {
+    if (index == 0) {
+        setBoard_04_pinout();
+    } else if (index == 1) {
+        setBoard_ua4c_pinout();
+    }
+}
