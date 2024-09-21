89a04c38a385fac883f7b8ffe695ab0f0dfa8d45
GitHub git update Action <action@github.com>
2024-09-21 00:17:55 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega100.h
+++ generated/controllers/generated/rusefi_generated_mega100.h
@@ -1234,34 +1234,22 @@
 #define pin_output_mode_e_OM_OPENDRAIN 2
 #define pin_output_mode_e_OM_OPENDRAIN_INVERTED 3
 #define PRIME_CURVE_COUNT 8
-#define PROTOCOL_ACR_NAME "acr"
 #define PROTOCOL_ANALOG_CHART "analog_chart"
-#define PROTOCOL_COIL1_SHORT_NAME "c1"
+#define PROTOCOL_COIL_SHORT_PREFIX "c"
 #define PROTOCOL_CRANK1 "t1"
 #define PROTOCOL_CRANK2 "t2"
 #define PROTOCOL_ENGINE_SNIFFER "wave_chart"
 #define PROTOCOL_ES_DOWN "d"
 #define PROTOCOL_ES_UP "u"
 #define PROTOCOL_HELLO_PREFIX "***"
-#define PROTOCOL_HIP_NAME "HIP"
-#define PROTOCOL_HPFP_NAME "hpfp"
-#define PROTOCOL_INJ1_SHORT_NAME "i1"
-#define PROTOCOL_INJ1_STAGE2_SHORT_NAME "j1"
+#define PROTOCOL_INJ_SHORT_PREFIX "i"
+#define PROTOCOL_INJ_STAGE2_SHORT_PREFIX "j"
 #define PROTOCOL_KNOCK_SPECTROGRAMM "knock_spectrogram"
 #define PROTOCOL_MSG "msg"
 #define PROTOCOL_OUTPIN "outpin"
 #define PROTOCOL_SIGNATURE_PREFIX "rusEFI "
-#define PROTOCOL_TACH_NAME "tach"
 #define PROTOCOL_TEST_RESPONSE_TAG "ts_p_alive"
 #define PROTOCOL_VERSION_TAG "rusEfiVersion"
-#define PROTOCOL_VVT1_NAME "VVT1"
-#define PROTOCOL_VVT2_NAME "VVT2"
-#define PROTOCOL_VVT3_NAME "VVT3"
-#define PROTOCOL_VVT4_NAME "VVT4"
-#define PROTOCOL_WA_CHANNEL_1 "input1"
-#define PROTOCOL_WA_CHANNEL_2 "input2"
-#define PROTOCOL_WA_CHANNEL_3 "input3"
-#define PROTOCOL_WA_CHANNEL_4 "input4"
 #define RANGE_INPUT_COUNT 6
 #define REBOOT_COMMAND 0xbb
 #define SCRIPT_CURVE_16 16
@@ -1302,7 +1290,7 @@
 #define show_Proteus_presets false
 #define show_test_presets false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 890822542
+#define SIGNATURE_HASH 2137807551
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1740,7 +1728,6 @@
 #define ts_show_trigger_comparator false
 #define ts_show_trigger_overrides true
 #define ts_show_tunerstudio_port true
-#define ts_show_uegoSerial true
 #define ts_show_vbatt true
 #define ts_show_vehicle_speed_sensor true
 #define ts_show_vehicle_weight false
@@ -1752,7 +1739,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.09.20.mega100.890822542"
+#define TS_SIGNATURE "rusEFI main.2024.09.21.mega100.2137807551"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_mega100.h
+++ generated/controllers/generated/signature_mega100.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 890822542
-#define TS_SIGNATURE "rusEFI main.2024.09.20.mega100.890822542"
+#define SIGNATURE_HASH 2137807551
+#define TS_SIGNATURE "rusEFI main.2024.09.21.mega100.2137807551"
