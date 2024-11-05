26b12c9bc1bf562fd67968db0a90de4c8c00e404
GitHub git update Action <action@github.com>
2024-11-05 00:17:52 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega100.h
+++ generated/controllers/generated/rusefi_generated_mega100.h
@@ -122,7 +122,7 @@
 #define BENCH_STARTER_DURATION 4000
 #define BENCH_VVT_DURATION 300
 #define blend_table_s_size 188
-#define BLOCKING_FACTOR 750
+#define BLOCKING_FACTOR 1024
 #define BOARD_CONSTANTS_EXTENSIONS_FROM_FILE 
 #define BOARD_CURVES_FROM_FILE 
 #define BOARD_DIAG_PANEL1_FROM_FILE 
@@ -1293,7 +1293,7 @@
 #define SentEtbType_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2141346511
+#define SIGNATURE_HASH 3491414355
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1570,6 +1570,8 @@
 #define TS_IO_TEST_COMMAND_char Z
 #define TS_ONLINE_PROTOCOL 'z'
 #define TS_ONLINE_PROTOCOL_char z
+#define TS_OUTPUT_ALL_COMMAND 'A'
+#define TS_OUTPUT_ALL_COMMAND_char A
 #define TS_OUTPUT_COMMAND 'O'
 #define TS_OUTPUT_COMMAND_char O
 #define TS_PAGE_COMMAND 'P'
@@ -1754,7 +1756,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.11.04.mega100.2141346511"
+#define TS_SIGNATURE "rusEFI main.2024.11.05.mega100.3491414355"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_mega100.h
+++ generated/controllers/generated/signature_mega100.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2141346511
-#define TS_SIGNATURE "rusEFI main.2024.11.04.mega100.2141346511"
+#define SIGNATURE_HASH 3491414355
+#define TS_SIGNATURE "rusEFI main.2024.11.05.mega100.3491414355"
