fd924d618332f9d121c8641575947d636624797a
rusefillc <48498823+rusefillc@users.noreply.github.com>
2024-11-19 20:01:54 -0500

    hiding some Lua

--- board.mk
+++ board.mk
@@ -13,3 +13,9 @@ DDEFS += -DRAM_UNUSED_SIZE=100
 
 # assign critical LED to a non-existent pin
 DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+
+
+# we do not have much Lua RAM, let's drop some fancy functions
+DDEFS += -DWITH_LUA_CONSUMPTION=FALSE
+DDEFS += -DWITH_LUA_PID=FALSE
+DDEFS += -DWITH_LUA_STOP_ENGINE=FALSE
