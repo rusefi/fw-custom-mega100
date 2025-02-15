b102e22ec477b12ca1e464db00f5156e099e70e3
Amri Karisma <amrikarisma@live.com>
2025-02-15 23:54:57 +0700

    add pinout from mega  legacy

--- firmware/pinouts.cpp
+++ firmware/pinouts.cpp
@@ -4,15 +4,28 @@
 #include "tunerstudio.h"
 
 static void setBoard_04_pinout() {
-    engineConfiguration->injectionPins[0] = Gpio::D8;
-    engineConfiguration->injectionPins[1] = Gpio::D9;
-    engineConfiguration->injectionPins[2] = Gpio::D10;
-    engineConfiguration->injectionPins[3] = Gpio::D11;
+	engineConfiguration->injectionPins[0] = Gpio::B15;
+	engineConfiguration->injectionPins[1] = Gpio::B14;
+	engineConfiguration->injectionPins[2] = Gpio::B12;
+	engineConfiguration->injectionPins[3] = Gpio::B13;
+
+	engineConfiguration->ignitionPins[0] = Gpio::E2;
+	engineConfiguration->ignitionPins[1] = Gpio::E3;
+	engineConfiguration->ignitionPins[2] = Gpio::C13;
+	engineConfiguration->ignitionPins[3] = Gpio::E7;
 }
 
 static void setBoard_ua4c_pinout() {
-    engineConfiguration->injectionPins[0] = Gpio::D8;
-    engineConfiguration->injectionPins[1] = Gpio::B15;
+	engineConfiguration->injectionPins[0] = Gpio::B15;
+	engineConfiguration->injectionPins[1] = Gpio::D8;
+	engineConfiguration->injectionPins[2] = Gpio::D9;
+	engineConfiguration->injectionPins[3] = Gpio::D10;
+
+	engineConfiguration->ignitionPins[0] = Gpio::E15;
+	engineConfiguration->ignitionPins[1] = Gpio::E4;
+	engineConfiguration->ignitionPins[2] = Gpio::D13;
+	engineConfiguration->ignitionPins[3] = Gpio::E5;
+
 }
 
 /*PUBLIC_API_WEAK*/ void boardTsAction(uint16_t index) {
