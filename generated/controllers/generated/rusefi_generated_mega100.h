75326fa8ec724c0d4194a8743952d1f9869af534
GitHub git update Action <action@github.com>
2025-03-12 00:19:19 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega100.h
+++ generated/controllers/generated/rusefi_generated_mega100.h
@@ -1230,6 +1230,8 @@
 #define maf_sensor_type_e_enum "v0", "v1", "v2", "v3"
 #define MAIN_HELP_URL "http://www.rusefi.com/"
 #define MAP_ANGLE_SIZE 8
+#define MAP_EST_LOAD_COUNT 16
+#define MAP_EST_RPM_COUNT 16
 #define MAP_sensor_config_s_size 140
 #define MAP_UPPER_LIMIT 1000
 #define MAP_WINDOW_SIZE 8
@@ -1381,7 +1383,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3693006204
+#define SIGNATURE_HASH 2348857981
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1876,7 +1878,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.03.11.mega100.3693006204"
+#define TS_SIGNATURE "rusEFI main.2025.03.12.mega100.2348857981"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_mega100.h
+++ generated/controllers/generated/signature_mega100.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3693006204
-#define TS_SIGNATURE "rusEFI main.2025.03.11.mega100.3693006204"
+#define SIGNATURE_HASH 2348857981
+#define TS_SIGNATURE "rusEFI main.2025.03.12.mega100.2348857981"
