aaf0cc6d194e55ced937ce7156b3986c81a15d3b
rusefillc <rusefillc@gmail.com>
2025-03-29 20:58:23 -0400

    fresh convention

--- board.mk
+++ board.mk
@@ -1,6 +1,5 @@
 BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
     $(BOARD_DIR)/firmware/pinouts.cpp \
-    $(BOARD_DIR)/default_tune.cpp \
 
 
 BOARDINC += $(BOARD_DIR)/generated/controllers/generated
