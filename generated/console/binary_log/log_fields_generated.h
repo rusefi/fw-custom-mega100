4e91dba745ad94deb30eda830c1ac5a2256873e3
GitHub git update Action <action@github.com>
2025-04-18 00:22:59 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/console/binary_log/log_fields_generated.h
+++ generated/console/binary_log/log_fields_generated.h
@@ -988,7 +988,13 @@ static const LogField fields[] = {
 	{getLiveData<electronic_throttle_s>(0)->m_wastegatePosition, "etb1DC: wastegatePosition", "%", 2, "ETB more"},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
+	{getLiveData<electronic_throttle_s>(0)->m_lastPidDtMs, "etb1EBT: last PID dT", "mS", 3, "ETB more"},
+#endif
+#if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
 	{getLiveData<electronic_throttle_s>(0)->etbFeedForward, "etb1etbFeedForward", "", 0},
+#endif
+#if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
+	{getLiveData<electronic_throttle_s>(0)->integralError, "etb1Integral error", "", 3, "ETB more"},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
 	{getLiveData<electronic_throttle_s>(0)->etbCurrentTarget, "etb1ETB: target for current pedal", "%", 3},
@@ -997,13 +1003,13 @@ static const LogField fields[] = {
 	{getLiveData<electronic_throttle_s>(0)->m_adjustedTarget, "etb1Adjusted target", "%", 2},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
-	{*getLiveData<electronic_throttle_s>(0), 28, 0, "etb1etbRevLimitActive", ""},
+	{*getLiveData<electronic_throttle_s>(0), 36, 0, "etb1etbRevLimitActive", ""},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
-	{*getLiveData<electronic_throttle_s>(0), 28, 1, "etb1jamDetected", ""},
+	{*getLiveData<electronic_throttle_s>(0), 36, 1, "etb1jamDetected", ""},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
-	{*getLiveData<electronic_throttle_s>(0), 28, 2, "etb1validPlantPosition", ""},
+	{*getLiveData<electronic_throttle_s>(0), 36, 2, "etb1validPlantPosition", ""},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
 	{getLiveData<electronic_throttle_s>(0)->etbTpsErrorCounter, "etb1ETB TPS error counter", "count", 0, "ETB more"},
@@ -1042,7 +1048,13 @@ static const LogField fields[] = {
 	{getLiveData<electronic_throttle_s>(1)->m_wastegatePosition, "etb2DC: wastegatePosition", "%", 2, "ETB more"},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
+	{getLiveData<electronic_throttle_s>(1)->m_lastPidDtMs, "etb2EBT: last PID dT", "mS", 3, "ETB more"},
+#endif
+#if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
 	{getLiveData<electronic_throttle_s>(1)->etbFeedForward, "etb2etbFeedForward", "", 0},
+#endif
+#if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
+	{getLiveData<electronic_throttle_s>(1)->integralError, "etb2Integral error", "", 3, "ETB more"},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
 	{getLiveData<electronic_throttle_s>(1)->etbCurrentTarget, "etb2ETB: target for current pedal", "%", 3},
@@ -1051,13 +1063,13 @@ static const LogField fields[] = {
 	{getLiveData<electronic_throttle_s>(1)->m_adjustedTarget, "etb2Adjusted target", "%", 2},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
-	{*getLiveData<electronic_throttle_s>(1), 28, 0, "etb2etbRevLimitActive", ""},
+	{*getLiveData<electronic_throttle_s>(1), 36, 0, "etb2etbRevLimitActive", ""},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
-	{*getLiveData<electronic_throttle_s>(1), 28, 1, "etb2jamDetected", ""},
+	{*getLiveData<electronic_throttle_s>(1), 36, 1, "etb2jamDetected", ""},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
-	{*getLiveData<electronic_throttle_s>(1), 28, 2, "etb2validPlantPosition", ""},
+	{*getLiveData<electronic_throttle_s>(1), 36, 2, "etb2validPlantPosition", ""},
 #endif
 #if EFI_PROD_CODE && EFI_ELECTRONIC_THROTTLE_BODY && FULL_SD_LOGS
 	{getLiveData<electronic_throttle_s>(1)->etbTpsErrorCounter, "etb2ETB TPS error counter", "count", 0, "ETB more"},
--- generated/live_data_generated/electronic_throttle_generated.h
+++ generated/live_data_generated/electronic_throttle_generated.h
@@ -29,173 +29,184 @@ struct electronic_throttle_s {
 	 */
 	float m_wastegatePosition = (float)0;
 	/**
+	 * "EBT: last PID dT"
+	 * units: mS
 	 * offset 16
+	 */
+	float m_lastPidDtMs = (float)0;
+	/**
+	 * offset 20
 	 */
 	percent_t etbFeedForward = (percent_t)0;
 	/**
+	 * Integral error
+	 * offset 24
+	 */
+	float integralError = (float)0;
+	/**
 	 * ETB: target for current pedal
 	 * units: %
-	 * offset 20
+	 * offset 28
 	 */
 	float etbCurrentTarget = (float)0;
 	/**
 	 * Adjusted target
 	 * units: %
-	 * offset 24
+	 * offset 32
 	 */
 	scaled_channel<int16_t, 100, 1> m_adjustedTarget = (int16_t)0;
 	/**
 	 * need 4 byte alignment
 	 * units: units
-	 * offset 26
+	 * offset 34
 	 */
-	uint8_t alignmentFill_at_26[2] = {};
+	uint8_t alignmentFill_at_34[2] = {};
 	/**
-	offset 28 bit 0 */
+	offset 36 bit 0 */
 	bool etbRevLimitActive : 1 {};
 	/**
-	offset 28 bit 1 */
+	offset 36 bit 1 */
 	bool jamDetected : 1 {};
 	/**
-	offset 28 bit 2 */
+	offset 36 bit 2 */
 	bool validPlantPosition : 1 {};
 	/**
-	offset 28 bit 3 */
-	bool unusedBit_11_3 : 1 {};
+	offset 36 bit 3 */
+	bool unusedBit_13_3 : 1 {};
 	/**
-	offset 28 bit 4 */
-	bool unusedBit_11_4 : 1 {};
+	offset 36 bit 4 */
+	bool unusedBit_13_4 : 1 {};
 	/**
-	offset 28 bit 5 */
-	bool unusedBit_11_5 : 1 {};
+	offset 36 bit 5 */
+	bool unusedBit_13_5 : 1 {};
 	/**
-	offset 28 bit 6 */
-	bool unusedBit_11_6 : 1 {};
+	offset 36 bit 6 */
+	bool unusedBit_13_6 : 1 {};
 	/**
-	offset 28 bit 7 */
-	bool unusedBit_11_7 : 1 {};
+	offset 36 bit 7 */
+	bool unusedBit_13_7 : 1 {};
 	/**
-	offset 28 bit 8 */
-	bool unusedBit_11_8 : 1 {};
+	offset 36 bit 8 */
+	bool unusedBit_13_8 : 1 {};
 	/**
-	offset 28 bit 9 */
-	bool unusedBit_11_9 : 1 {};
+	offset 36 bit 9 */
+	bool unusedBit_13_9 : 1 {};
 	/**
-	offset 28 bit 10 */
-	bool unusedBit_11_10 : 1 {};
+	offset 36 bit 10 */
+	bool unusedBit_13_10 : 1 {};
 	/**
-	offset 28 bit 11 */
-	bool unusedBit_11_11 : 1 {};
+	offset 36 bit 11 */
+	bool unusedBit_13_11 : 1 {};
 	/**
-	offset 28 bit 12 */
-	bool unusedBit_11_12 : 1 {};
+	offset 36 bit 12 */
+	bool unusedBit_13_12 : 1 {};
 	/**
-	offset 28 bit 13 */
-	bool unusedBit_11_13 : 1 {};
+	offset 36 bit 13 */
+	bool unusedBit_13_13 : 1 {};
 	/**
-	offset 28 bit 14 */
-	bool unusedBit_11_14 : 1 {};
+	offset 36 bit 14 */
+	bool unusedBit_13_14 : 1 {};
 	/**
-	offset 28 bit 15 */
-	bool unusedBit_11_15 : 1 {};
+	offset 36 bit 15 */
+	bool unusedBit_13_15 : 1 {};
 	/**
-	offset 28 bit 16 */
-	bool unusedBit_11_16 : 1 {};
+	offset 36 bit 16 */
+	bool unusedBit_13_16 : 1 {};
 	/**
-	offset 28 bit 17 */
-	bool unusedBit_11_17 : 1 {};
+	offset 36 bit 17 */
+	bool unusedBit_13_17 : 1 {};
 	/**
-	offset 28 bit 18 */
-	bool unusedBit_11_18 : 1 {};
+	offset 36 bit 18 */
+	bool unusedBit_13_18 : 1 {};
 	/**
-	offset 28 bit 19 */
-	bool unusedBit_11_19 : 1 {};
+	offset 36 bit 19 */
+	bool unusedBit_13_19 : 1 {};
 	/**
-	offset 28 bit 20 */
-	bool unusedBit_11_20 : 1 {};
+	offset 36 bit 20 */
+	bool unusedBit_13_20 : 1 {};
 	/**
-	offset 28 bit 21 */
-	bool unusedBit_11_21 : 1 {};
+	offset 36 bit 21 */
+	bool unusedBit_13_21 : 1 {};
 	/**
-	offset 28 bit 22 */
-	bool unusedBit_11_22 : 1 {};
+	offset 36 bit 22 */
+	bool unusedBit_13_22 : 1 {};
 	/**
-	offset 28 bit 23 */
-	bool unusedBit_11_23 : 1 {};
+	offset 36 bit 23 */
+	bool unusedBit_13_23 : 1 {};
 	/**
-	offset 28 bit 24 */
-	bool unusedBit_11_24 : 1 {};
+	offset 36 bit 24 */
+	bool unusedBit_13_24 : 1 {};
 	/**
-	offset 28 bit 25 */
-	bool unusedBit_11_25 : 1 {};
+	offset 36 bit 25 */
+	bool unusedBit_13_25 : 1 {};
 	/**
-	offset 28 bit 26 */
-	bool unusedBit_11_26 : 1 {};
+	offset 36 bit 26 */
+	bool unusedBit_13_26 : 1 {};
 	/**
-	offset 28 bit 27 */
-	bool unusedBit_11_27 : 1 {};
+	offset 36 bit 27 */
+	bool unusedBit_13_27 : 1 {};
 	/**
-	offset 28 bit 28 */
-	bool unusedBit_11_28 : 1 {};
+	offset 36 bit 28 */
+	bool unusedBit_13_28 : 1 {};
 	/**
-	offset 28 bit 29 */
-	bool unusedBit_11_29 : 1 {};
+	offset 36 bit 29 */
+	bool unusedBit_13_29 : 1 {};
 	/**
-	offset 28 bit 30 */
-	bool unusedBit_11_30 : 1 {};
+	offset 36 bit 30 */
+	bool unusedBit_13_30 : 1 {};
 	/**
-	offset 28 bit 31 */
-	bool unusedBit_11_31 : 1 {};
+	offset 36 bit 31 */
+	bool unusedBit_13_31 : 1 {};
 	/**
 	 * "ETB TPS error counter"
 	 * units: count
-	 * offset 32
+	 * offset 40
 	 */
 	uint16_t etbTpsErrorCounter = (uint16_t)0;
 	/**
 	 * "ETB pedal error counter"
 	 * units: count
-	 * offset 34
+	 * offset 42
 	 */
 	uint16_t etbPpsErrorCounter = (uint16_t)0;
 	/**
-	 * offset 36
+	 * offset 44
 	 */
 	int8_t etbErrorCode = (int8_t)0;
 	/**
-	 * offset 37
+	 * offset 45
 	 */
 	int8_t etbErrorCodeBlinker = (int8_t)0;
 	/**
 	 * ETB traction control
 	 * units: %
-	 * offset 38
+	 * offset 46
 	 */
 	int8_t tcEtbDrop = (int8_t)0;
 	/**
 	 * need 4 byte alignment
 	 * units: units
-	 * offset 39
+	 * offset 47
 	 */
-	uint8_t alignmentFill_at_39[1] = {};
+	uint8_t alignmentFill_at_47[1] = {};
 	/**
 	 * ETB jam timer
 	 * units: sec
-	 * offset 40
+	 * offset 48
 	 */
 	scaled_channel<uint16_t, 100, 1> jamTimer = (uint16_t)0;
 	/**
 	 * ETB with adjustments
 	 * units: %
-	 * offset 42
+	 * offset 50
 	 */
 	int8_t adjustedEtbTarget = (int8_t)0;
 	/**
-	 * offset 43
+	 * offset 51
 	 */
 	uint8_t state = (uint8_t)0;
 };
-static_assert(sizeof(electronic_throttle_s) == 44);
+static_assert(sizeof(electronic_throttle_s) == 52);
 
 // end
 // this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) controllers/actuators/electronic_throttle.txt
