ad9ea3b2bfd4d6cecf8a7aa2ff9b520565722d7b
GitHub git update Action <action@github.com>
2025-05-03 00:20:01 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega100.h
+++ generated/controllers/generated/rusefi_generated_mega100.h
@@ -1418,7 +1418,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2399613779
+#define SIGNATURE_HASH 2797764663
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1923,7 +1923,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.05.02.mega100.2399613779"
+#define TS_SIGNATURE "rusEFI main.2025.05.03.mega100.2797764663"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_mega100.h
+++ generated/controllers/generated/signature_mega100.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2399613779
-#define TS_SIGNATURE "rusEFI main.2025.05.02.mega100.2399613779"
+#define SIGNATURE_HASH 2797764663
+#define TS_SIGNATURE "rusEFI main.2025.05.03.mega100.2797764663"
