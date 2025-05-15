34b0d80282dd0f47b4e60e2624459b3a8079c101
GitHub git update Action <action@github.com>
2025-05-15 00:20:27 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega100.h
+++ generated/controllers/generated/rusefi_generated_mega100.h
@@ -1426,7 +1426,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 734979719
+#define SIGNATURE_HASH 296875807
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1757,6 +1757,7 @@
 #define ts_show_analog_divider true
 #define ts_show_analog_ego true
 #define ts_show_analog_input_settings true
+#define ts_show_analog_o2 true
 #define ts_show_aux_connections true
 #define ts_show_aux_sensors true
 #define ts_show_auxserial_pins true
@@ -1764,6 +1765,7 @@
 #define ts_show_bank2_cam2 true
 #define ts_show_brake_pedal_indicator true
 #define ts_show_brake_pedal_pin true
+#define ts_show_cam1 true
 #define ts_show_cam2 true
 #define ts_show_can2 true
 #define ts_show_can_bitrate true
@@ -1797,6 +1799,7 @@
 #define ts_show_debug_mode false
 #define ts_show_ego2 true
 #define ts_show_egt true
+#define ts_show_engine_make true
 #define ts_show_etb true
 #define ts_show_etb_bias_curve true
 #define ts_show_etb_extra_monitoring 
@@ -1938,14 +1941,14 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.05.14.mega100.734979719"
+#define TS_SIGNATURE "rusEFI main.2025.05.15.mega100.296875807"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
 #define TS_TEST_COMMAND_char t
-#define TS_TOTAL_OUTPUT_SIZE 1852
+#define TS_TOTAL_OUTPUT_SIZE 1860
 #define TS_TRIGGER_SCOPE_CHANNEL_1_NAME "Channel 1"
 #define TS_TRIGGER_SCOPE_CHANNEL_2_NAME "Channel 2"
 #define TS_TRIGGER_SCOPE_DISABLE 5
--- generated/controllers/generated/signature_mega100.h
+++ generated/controllers/generated/signature_mega100.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 734979719
-#define TS_SIGNATURE "rusEFI main.2025.05.14.mega100.734979719"
+#define SIGNATURE_HASH 296875807
+#define TS_SIGNATURE "rusEFI main.2025.05.15.mega100.296875807"
