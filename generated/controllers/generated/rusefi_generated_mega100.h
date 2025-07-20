2b4d38ac6e928a48ec7085e435c45aab43a35628
GitHub git update Action <action@github.com>
2025-07-20 00:25:41 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega100.h
+++ generated/controllers/generated/rusefi_generated_mega100.h
@@ -1498,7 +1498,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 4005974880
+#define SIGNATURE_HASH 3678575316
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2040,7 +2040,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.07.19.mega100.4005974880"
+#define TS_SIGNATURE "rusEFI main.2025.07.20.mega100.3678575316"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_mega100.h
+++ generated/controllers/generated/signature_mega100.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4005974880
-#define TS_SIGNATURE "rusEFI main.2025.07.19.mega100.4005974880"
+#define SIGNATURE_HASH 3678575316
+#define TS_SIGNATURE "rusEFI main.2025.07.20.mega100.3678575316"
